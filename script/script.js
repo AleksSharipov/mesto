d1b8407b32dd516f870a96e6c772aa0d7d9c11bd
Aleksandr Sharipov <sharipov.sasha.spb@yandex.ru>
2020-12-25 01:12:49 +0300

    Исправлены все недочеты

--- script/script.js
+++ script/script.js
@@ -1,36 +1,46 @@
-let refactoringBtn = document.querySelector('.profile__button_refactoring');
+let refactoringBtn = document.querySelector('.profile__button_refactoring_avatar');
 let popupRefactoring = document.querySelector('.popup');
 let closeBtn = document.querySelector('.popup__close');
 let popupForm = popupRefactoring.querySelector('.popup__form');
-let refName = popupRefactoring.querySelector('.popup__form-input_name');
-let refWork = popupRefactoring.querySelector('.popup__form-input_work');
+let refName = popupRefactoring.querySelector('.popup__form_name');
+let refWork = popupRefactoring.querySelector('.popup__form_work');
 
 let profileName = document.querySelector('.profile__title');
 let profileDescr = document.querySelector('.profile__description');
 
-function newPopup(popup) {
-  popup.classList.toggle('popup_opened');
+// function newPopup(popup) {
+//   popup.classList.toggle('popup_opened');
+//   // refName.value = profileName.textContent;
+//   // refWork.value = profileDescr.textContent;
+// }
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
   refName.value = profileName.textContent;
   refWork.value = profileDescr.textContent;
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 popupForm.addEventListener('submit', function (e) {
   e.preventDefault();
   profileName.textContent = refName.value;
   profileDescr.textContent = refWork.value;
-  newPopup(popupRefactoring);
+  closePopup(popupRefactoring);
 })
 
 refactoringBtn.addEventListener('click', function () {
-  newPopup(popupRefactoring);
+  openPopup(popupRefactoring);
 })
 
 closeBtn.addEventListener('click', function () {
-  newPopup(popupRefactoring);
+  closePopup(popupRefactoring);
 })
 
 popupRefactoring.addEventListener('click', function (e) {
   if (e.target === popupRefactoring) {
-    newPopup(popupRefactoring);
+    closePopup(popupRefactoring);
   }
 })
